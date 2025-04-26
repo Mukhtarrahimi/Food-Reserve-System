92f87d93d87e1d2c066c8ccb52c05aa2d1f700d4
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-04-26 13:07:12 +0330

    The Student class was created and its private members were written.

--- app.cpp
+++ app.cpp
@@ -4,6 +4,17 @@
 #include <ctime>
 using namespace std;
 
+// --------------------- Student Class Section -----------------
+class Student {
+private:
+    int userId;
+    string studentId;
+    string name;
+    string email;
+    float balance;
+    bool isActive;
+public:
+};
 int main() {
  
     return 0;
