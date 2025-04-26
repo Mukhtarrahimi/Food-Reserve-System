c77f029411069c91a20374e66dde338f0063a165
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-04-26 22:31:55 +0330

    diningHall class code update.

--- app.cpp
+++ app.cpp
@@ -329,62 +329,72 @@ cout << endl;
 
 }
 
-// ------------- DiningHall  Class Section -------------
-class DiningHall{
-    private:
-        int hallId;
-        string name;
-        string address;
-        int capicity;
-    public:
-        DiningHall();
-        DiningHall(int, string, string, int);
-        void sethallId(int);
-        int gethallId() const;
-        void sethallName(const string&);
-        string gethallName()const;
-        void setAddress(const string&);
-        string getAddress()const;
-        void setCapicity(int);
-        int getCapicity()const;
-
+// ------------- DiningHall Class Section -------------
+class DiningHall {
+private:
+    int hallId;
+    string name;
+    string address;
+    int capacity;
+public:
+    DiningHall();
+    DiningHall(int, string, string, int);
+    void setHallId(int);
+    int getHallId() const;
+    void setHallName(const string&);
+    string getHallName() const;
+    void setAddress(const string&);
+    string getAddress() const;
+    void setCapacity(int);
+    int getCapacity() const;
 };
-DiningHall::DiningHall(){
+
+// Constructors
+DiningHall::DiningHall() {
     hallId = 0;
     name = "";
     address = "";
-    capicity = 0;
+    capacity = 0;
 }
-DiningHall::DiningHall(int hid, string hn, string ad, int cp){
+
+DiningHall::DiningHall(int hid, string hn, string ad, int cp) {
     hallId = hid;
     name = hn;
     address = ad;
-    capicity = cp;
+    capacity = cp;
 }
-// setter & getter
-void DiningHall::sethallId(int id){
+
+// Setter & Getter
+void DiningHall::setHallId(int id) {
     hallId = id;
 }
-int DiningHall::gethallId() const{
+
+int DiningHall::getHallId() const {
     return hallId;
 }
-void DiningHall::sethallName(const string& n){
+
+void DiningHall::setHallName(const string& n) {
     name = n;
 }
-string DiningHall::gethallName() const{
+
+string DiningHall::getHallName() const {
     return name;
 }
-void DiningHall::setAddress(const string& ad){
+
+void DiningHall::setAddress(const string& ad) {
     address = ad;
 }
-string DiningHall::getAddress()const{
+
+string DiningHall::getAddress() const {
     return address;
 }
-void DiningHall::setCapicity(int cp){
-    capicity = cp;
+
+void DiningHall::setCapacity(int cp) {
+    capacity = cp;
 }
-int DiningHall::getCapicity()const{
-    return capicity;
+
+int DiningHall::getCapacity() const {
+    return capacity;
 }
 
 int main() {
