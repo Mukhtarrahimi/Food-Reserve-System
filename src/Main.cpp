11f055a29a785679b8641aded9e0021fb85d78f4
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-04-28 14:47:55 +0330

    all code update and finished and set example in main file for comile.

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,5 +5,11 @@
 using namespace std;
 
 int main() {
+    Student s1(1, "R123", "Mukhtar Rahimi", "Rahimi110@gmail.com", 100.0, true);
+    DiningHall hall1(1, "Main Hall", "Tarbiat Street", 1000);
+    Meal meal1(1, "steek", 60.0, MealType::Lunch);
+
+    s1.reserveMeal(meal1, hall1);
+    s1.print();
     return 0;
 }
