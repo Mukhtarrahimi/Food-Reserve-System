ab3a802a5998fa0b189a01940f1fb80cafe79536
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-05-07 15:46:23 +0330

    user.h file added to projec.

---
+++ src/User.h
@@ -0,0 +1,31 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+#include <regex>
+
+using namespace std;
+
+class User {
+protected:
+    string name;
+    string email;
+    string studentID;
+
+public:
+    User(string userName, string userEmail, string userStudentID);
+
+    string getName() const;
+    string getEmail() const;
+    string getStudentID() const;
+
+    void setName(const string& userName);
+    void setEmail(const string& userEmail);
+    void setStudentID(const string& userStudentID);
+
+    virtual void reserveMeal();
+
+    void displayUserInfo() const;
+};
+
+#endif
