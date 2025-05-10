5a47b30138673d2e8aa8e4d33065ebd546975301
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-05-10 15:44:50 +0330

    panel.cpp file added to project.

---
+++ src/Panel.cpp
@@ -0,0 +1,14 @@
+#include "Panel.h"
+#include <iostream>
+
+void Panel::viewReservations() {
+    // View reservations logic
+}
+
+void Panel::showMenu() {
+    // Show menu logic
+}
+
+void Panel::exit() {
+    std::cout << "Exiting program." << std::endl;
+}
